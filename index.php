a0e2c9bfb69ad6954f37e81680b450cd4cdf43f0
rickysandy <95896044+rickysandy@users.noreply.github.com>
2021-12-10 11:06:25 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html> 
 	<head> 
-	<title>Hellp PHP World 2</title>
+	<title>Hellp PHP World plz die soon</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP 2021  Test</h1>
-		<p><b>  Sandeep Singh Changed the index file</b></p>
+		<p><b>  Sandeep Singh Changing the programming language from PHP to JAVA file</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
-	<h2>PHP Information VERSION1 </h2> 
+	<h2>PHP Information VERSION-3 </h2> 
 		<p> <?php phpinfo(); ?> </p> 
 	</body> 
 </html>
